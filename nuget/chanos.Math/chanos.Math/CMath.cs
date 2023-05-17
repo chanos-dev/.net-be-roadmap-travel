fddf375df42fe31500ab97c730d8dc8383b5412b
chanos-dev <chanos.dev@gmail.com>
2023-05-18 02:23:52 +0900

    upload nuget package sample

---
+++ nuget/chanos.Math/chanos.Math/CMath.cs
@@ -0,0 +1,10 @@
+﻿namespace CMath
+{
+    public class CMath
+    {
+        public int Add(int a, int b) => a + b;
+        public int Sub(int a, int b) => a - b;
+        public int Mul(int a, int b) => a * b;
+        public int Div(int a, int b) => a / b;
+    }
+}
